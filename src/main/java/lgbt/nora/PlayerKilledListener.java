56317bcbd7e012ff2324152fbb5c2efc4b036e6f
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-08-05 21:45:50 +0200

    feat: Add applyEffectsToEveryone and announceHuntedDeath

--- src/main/java/lgbt/nora/PlayerKilledListener.java
+++ src/main/java/lgbt/nora/PlayerKilledListener.java
@@ -15,7 +15,11 @@ import java.util.Objects;
 import java.util.Random;
 
 public class PlayerKilledListener implements Listener {
-  // Full list of "positive" effects:
+  ///////////////////////////////////////////////////////////////////////////
+  ///////                        BEGIN CONFIG                         ///////
+  /////// IF YOU DO NOT UNDERSTAND AN OPTION HERE, DO NOT TO TOUCH IT ///////
+  ///////////////////////////////////////////////////////////////////////////
+  // Full list of "positive" effects (Selected unless in excluded):
   List<PotionEffectType> positiveEffects = Arrays.asList(PotionEffectType.HEAL, PotionEffectType.SPEED, PotionEffectType.FAST_DIGGING, PotionEffectType.INCREASE_DAMAGE, PotionEffectType.JUMP, PotionEffectType.REGENERATION, PotionEffectType.DAMAGE_RESISTANCE, PotionEffectType.FIRE_RESISTANCE, PotionEffectType.WATER_BREATHING, PotionEffectType.INVISIBILITY, PotionEffectType.HEALTH_BOOST, PotionEffectType.ABSORPTION, PotionEffectType.SATURATION, PotionEffectType.LUCK, PotionEffectType.SLOW_FALLING, PotionEffectType.CONDUIT_POWER, PotionEffectType.DOLPHINS_GRACE, PotionEffectType.HERO_OF_THE_VILLAGE, PotionEffectType.GLOWING, PotionEffectType.LEVITATION); // levitation cuz celestial suggested it cuz funni
   // List of effects to exclude from the above:
   List<PotionEffectType> excluded = Arrays.asList(PotionEffectType.HEAL, PotionEffectType.GLOWING);
@@ -27,14 +31,19 @@ public class PlayerKilledListener implements Listener {
   public static List<String> hunted = Arrays.asList("N","ilovemorbius","");
   // Should we give everyone the effect?
   public static boolean applyEffectsToEveryone = false;
+  // Should we announce when the hunted person dies?
+  public static boolean announceHuntedDeath = false;
   // Time (unknown measurement) for ShortTimed
   public static int shortTimedTime = 256;
   // Time (unknown measurement) for Timed
   public static int timedTime = shortTimedTime*10;
   // Time (unknown measurement) for non-timed effects
   public static int untimedTime = timedTime*15;
-  /////// BEGIN FUNCTIONALITY
 
+  ////////////////////////////////////////////////////////////////////////////
+  ///////                    BEGIN FUNCTIONALITY                       ///////
+  /////// DO NOT TOUCH ANYTHING BELOW HERE UNLESS YK WHAT YOU'RE DOING ///////
+  ////////////////////////////////////////////////////////////////////////////
   // Check if a potion is in a specified array
   public boolean PotionIsInArray(PotionEffectType type, List<PotionEffectType> list)
   {
@@ -57,29 +66,29 @@ public class PlayerKilledListener implements Listener {
     Player victim = e.getEntity();
     if (victim.getKiller() != null) {
       Player killer = victim.getKiller();
-      if (killer != victim && StringIsInArray(killer.getName(),hunted)) {
+      if (killer != victim && (applyEffectsToEveryone || StringIsInArray(killer.getName(),hunted))) {
         // killer = Hunted
         // victim = Hunter
         Random r = new Random();
         PotionEffectType eff = null;
-        while (eff == null || PotionIsInArray(eff, excluded)) {
+        while (eff == null || PotionIsInArray(eff, excluded)) { // Repeat Effect Loop until applicable event found
           int idx = r.nextInt(positiveEffects.size());
           eff = positiveEffects.get(idx);
         }
+        // Define Effect Duration
         int dur = PotionIsInArray(eff, shortTimed) ? shortTimedTime : PotionIsInArray(eff, timed) ? timedTime : untimedTime;
+        // Define Amplifier as 0
         int amp = 0;
+        // If the player has an effect, increase their effect amplifier by one
         if (killer.hasPotionEffect(eff))
           amp = Objects.requireNonNull(killer.getPotionEffect(eff)).getAmplifier() + 1;
+        // Create Effect Instance
         PotionEffect effect = new PotionEffect(eff, dur, amp).withParticles(false).withIcon(true);
+        // Add the potion effect
         killer.addPotionEffect(effect);
-        //for (Player allPlayers : Bukkit.getOnlinePlayers())
-          //if (allPlayers != killer) {
-            //int idx = r.nextInt(Messages.size());
-            //String el = Messages.get(idx);//§
-            //allPlayers.sendMessage(ChatColor.translateAlternateColorCodes('&',"&8&k727&r &o&8" + el + "&r&8&k727&r"));
-          //} else {
+        // Send the player a message regarding the type of effect they got
         killer.sendMessage(ChatColor.translateAlternateColorCodes('&',"&8&k727&r &a&oYour power increases... &2&oYou got &9&o" + effect.getType().getName() + " &r&8&k727&r"));
-      } else if (StringIsInArray(killer.getName(),hunted))
+      } else if (StringIsInArray(killer.getName(),hunted) && announceHuntedDeath) // Tell everyone that they got defeated
         for (Player plr: Bukkit.getOnlinePlayers())
           plr.sendMessage(ChatColor.translateAlternateColorCodes('&',"&8&k727&r &a"+hunted+" has been defeated by " + killer.getName() + " &8&k727&r"));
     }
